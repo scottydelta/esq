66a283c484e693f7a09bb73ea7fb963ffbca914f
scottydelta <scottydelta@users.noreply.github.com>
2022-05-01 20:49:46 +0530

    fix travis

--- setup.py
+++ setup.py
@@ -39,6 +39,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/scottydelta/esq',
-    version='0.1.0',
+    version='0.1.3',
     zip_safe=False,
 )
