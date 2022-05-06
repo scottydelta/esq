ffda132b2168b2e3b628c4bb4f43d601e176eeb9
scottydelta <scottydelta@users.noreply.github.com>
2022-05-07 00:16:33 +0530

    added Esq class and queries generator

--- esq/__init__.py
+++ esq/__init__.py
@@ -2,4 +2,23 @@
 
 __author__ = """Vikash Bajaj"""
 __email__ = 'contact@vikashbajaj.com'
-__version__ = '0.1.0'
+__version__ = '0.1.10'
+
+
+import json
+from .es_queries_generator import (
+    label_value_query_generator,
+    coordinates_query_generator,
+    timeseries_query_generator,
+    docs_query_generator,
+)
+
+
+"""Main module."""
+class Esq:  
+      
+    def __init__(self, elasticsearch_client):  
+        self.elasticsearch_client = elasticsearch_client  
+      
+
+
