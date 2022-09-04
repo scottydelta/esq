a033153314d7536f6117c39d92eb5cb4b5e7183e
scottydelta <scottydelta@users.noreply.github.com>
2022-09-05 01:28:46 +0800

    added first functional method to Esq class

--- esq/__init__.py
+++ esq/__init__.py
@@ -4,20 +4,28 @@ __author__ = """Vikash Bajaj"""
 __email__ = 'esq@vikashbajaj.com'
 __version__ = '1.0.1'
 
-
-import json
-from .es_queries_generator import (
-    label_value_query_generator,
-    coordinates_query_generator,
-    timeseries_query_generator,
-    docs_query_generator,
-)
-
-
+from .fetch_es_data import (fetch_aggr_data)
 """Main module."""
 
 
 class Esq:
 
     def __init__(self, elasticsearch_client):
-        self.elasticsearch_client = elasticsearch_client
+        try:
+            if elasticsearch_client.ping():
+                self.elasticsearch_client = elasticsearch_client
+            else:
+                raise ConnectionError("Cannot connect to the Elasticsearch, \
+                        check the Elasticsearch object and try again.")
+        except:
+            raise ConnectionError("Cannot connect to the Elasticsearch, \
+                    check the Elasticsearch object and try again.")
+
+    def get_aggr_data(self,
+                      index,
+                      data_type,
+                      data_field,
+                      size=100,
+                      filters=[]):
+        return fetch_aggr_data(self.elasticsearch_client, index, data_type,
+                               data_field, size, filters)
